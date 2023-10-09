aa9a6e823c283e36a8bb7f9d66331592f0248df3
Yi Heng <yihengwang2000@gmail.com>
2023-10-08 23:12:28 -0400

    Bunch of changes, implement CLI functionality

--- scraper.go
+++ scraper.go
@@ -4,7 +4,7 @@ import (
 	// "encoding/csv" 
 	"github.com/gocolly/colly" 
 	// "log" 
-	// "os"
+	"flag"
 	"strconv"
 	"fmt"
 	"time"
@@ -19,7 +19,7 @@ type PlayerRecord struct {
 func singleThreadedQuerying() {
 	var playerRecords []PlayerRecord
 	c := colly.NewCollector()
-	for i := 1; i <= 100; i++ {
+	for i := 1; i <= 30; i++ {
 		var link string ="https://battlefieldtracker.com/bf2042/leaderboards/stats/all/default?page=" + strconv.Itoa(i)
 		c.Visit(link)
 		c.OnHTML("tr", func(e *colly.HTMLElement) {
@@ -31,7 +31,7 @@ func singleThreadedQuerying() {
 func multiThreadedQuerying() {
 	var playerRecords []PlayerRecord
 	c := colly.NewCollector()
-	for i := 1; i <= 100; i++ {
+	for i := 1; i <= 30; i++ {
 		var link string ="https://battlefieldtracker.com/bf2042/leaderboards/stats/all/default?page=" + strconv.Itoa(i)
 		c.Visit(link)
 		c.OnHTML("tr", func(e *colly.HTMLElement) {
@@ -57,7 +57,16 @@ func callback(e *colly.HTMLElement, playerRecords []PlayerRecord, isGoRoutine bo
 }
 
 func main() {
-	// recordTime(multiThreadedQuerying, "multi-threaded querying")
-	recordTime(singleThreadedQuerying, "single-threaded querying")
-	recordTime(multiThreadedQuerying, "multi-threaded querying")
+	// sgf stands for "single-threaded querying first"
+	stqfPtr := flag.Bool("stqf", false, "")
+	flag.Parse() 
+	if *stqfPtr == true {
+		fmt.Println("Single-Threaded Querying First")
+		recordTime(singleThreadedQuerying, "single-threaded querying")
+		recordTime(multiThreadedQuerying, "multi-threaded querying")
+	} else {
+		fmt.Println("Multi-Threaded Querying First")
+		recordTime(multiThreadedQuerying, "multi-threaded querying")
+		recordTime(singleThreadedQuerying, "single-threaded querying")
+	}
 }
